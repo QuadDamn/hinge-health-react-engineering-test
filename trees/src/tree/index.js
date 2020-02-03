8892fcf780aee9f4f559a0410b0e2c0569449dc3
Shane Jeffery <nobull.wow@gmail.com>
2020-02-02 22:36:13 -0800

    Adding in nested ordered lists per bonus #2 part 1.

--- trees/src/tree/index.js
+++ trees/src/tree/index.js
@@ -5,13 +5,26 @@ import axios from 'axios'
 export default function Tree() {
     return (
         <div className="tree">
-            <div>root</div>
-            <div className="indent-first-child">ant</div>
-            <div className="indent-first-child">bear</div>
-            <div className="indent-second-child">cat</div>
-            <div className="indent-second-child">dog</div>
-            <div className="indent-third-child">elephant</div>
-            <div className="indent-first-child">frog</div>
+            <ol>
+                <li>root
+                    <ol>
+                        <li className="indent-first-child">ant</li>
+                        <li className="indent-first-child">bear
+                            <ol>
+                                <li className="indent-second-child">cat</li>
+                                <li className="indent-second-child">dog
+
+                                    <ol>
+                                        <li className="indent-third-child">elephant</li>
+                                    </ol>
+
+                                </li>
+                            </ol>
+                        </li>
+                        <li className="indent-first-child">frog</li>
+                    </ol>
+                </li>
+            </ol>
         </div>
     );
 }
