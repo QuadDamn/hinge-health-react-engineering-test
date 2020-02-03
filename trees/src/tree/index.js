77979f03ba55577e0bf4234b1d3a7ba2da1beec8
Shane Jeffery <nobull.wow@gmail.com>
2020-02-02 22:34:30 -0800

    Fixing styling of tree and removing spaces.

--- trees/src/tree/index.js
+++ trees/src/tree/index.js
@@ -4,14 +4,14 @@ import axios from 'axios'
 
 export default function Tree() {
     return (
-            <div className="tree">
-                root<br/>
-                &nbsp;&nbsp;&nbsp;&nbsp;ant <br/>
-                &nbsp;&nbsp;&nbsp;&nbsp;bear <br/>
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;cat <br/>
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;dog <br/>
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;elephant <br/>
-                &nbsp;&nbsp;&nbsp;&nbsp;frog <br/>
-            </div>
-    )
+        <div className="tree">
+            <div>root</div>
+            <div className="indent-first-child">ant</div>
+            <div className="indent-first-child">bear</div>
+            <div className="indent-second-child">cat</div>
+            <div className="indent-second-child">dog</div>
+            <div className="indent-third-child">elephant</div>
+            <div className="indent-first-child">frog</div>
+        </div>
+    );
 }
