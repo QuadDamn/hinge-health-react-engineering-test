81fe17e64d3a9a072002846a9b3bff43a051dd6f
Shane Jeffery <nobull.wow@gmail.com>
2020-02-03 23:01:30 -0800

    Added in component recursion to handle the tree being built with the JSON file data and then did the bonus for adding the periods for the different levels.

---
+++ trees/src/tree/TreeBranch.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './index.css';
+
+export default function TreeBranch({treeData, level}) {
+    function hasChildren(branch) {
+        return branch.children && branch.children.length;
+    }
+
+    function addPeriods(animalName) {
+        const periodString = '.';
+        const levelPeriods = periodString.repeat(level);
+        return [animalName.slice(0, 1), levelPeriods, animalName.slice(1)].join('');
+    }
+
+    return (
+        <ol>
+            {treeData.map((branch, i) => {
+            return <div key={`level-${level}-${i}`}>
+                <li className={(branch.name !== 'root') ? `indent-level-${level}-child` : ''}>
+                    {addPeriods(branch.name)}
+                    {hasChildren(branch) && <TreeBranch treeData={branch.children} level={level + 1} />}
+                </li>
+            </div>
+        })}
+        </ol>
+    );
+}
--- trees/src/tree/index.js
+++ trees/src/tree/index.js
@@ -1,30 +1,15 @@
 import React, {useState, useEffect} from 'react';
 import './index.css'
 import axios from 'axios'
+import data from './data.json';
+import TreeBranch from "./TreeBranch";
 
 export default function Tree() {
+    const [treeData, setTreeData] = useState(data);
+
     return (
         <div className="tree">
-            <ol>
-                <li>root
-                    <ol>
-                        <li className="indent-first-child">ant</li>
-                        <li className="indent-first-child">bear
-                            <ol>
-                                <li className="indent-second-child">cat</li>
-                                <li className="indent-second-child">dog
-
-                                    <ol>
-                                        <li className="indent-third-child">elephant</li>
-                                    </ol>
-
-                                </li>
-                            </ol>
-                        </li>
-                        <li className="indent-first-child">frog</li>
-                    </ol>
-                </li>
-            </ol>
+            <TreeBranch treeData={treeData} level={0} />
         </div>
     );
 }
